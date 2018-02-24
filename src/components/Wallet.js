bdb3630d5c64f98af354ac4ab6a5ab6bc5950d98
Simon Weller <s.meierbeck@gmail.com>
2018-02-24 14:47:28 +0100

    connected wallet and tested that actions are dispatched

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { deposit, withdraw } from '../actions/balance';
 
 export class Wallet extends Component {
+    state = {amount: undefined}
+
+    updateAmount = event => {
+        const amount = parseInt(event.target.value, 10);
+        this.setState({amount});
+    }
+
+    deposit = () => {
+        this.props.deposit(this.state.amount);
+    }
+
+    withdraw = () => {
+        this.props.withdraw(this.state.amount);
+    }
+
     render() {
         return (
             <div>
                 <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+                <br />
+                <input type="text" className="input-wallet" onChange={this.updateAmount}/>
+                <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+                <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
             </div>
         );
     }
@@ -17,4 +37,11 @@ const mapStateToProps = state => {
     };
 }
 
-export default connect(mapStateToProps)(Wallet);
+const mapDispatchToProps = dispatch => {
+    return {
+        deposit: amount => dispatch(deposit(amount)),
+        withdraw: amount => dispatch(withdraw(amount))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Wallet from './Wallet';
+import configureStore from 'redux-mock-store';
+import * as actionTypes from '../actions/actionTypes'
 
 describe('Wallet', () => {
-    const store = {
-        getState: () => 20,
-        subscribe: () => {},
-        dispatch: () => {}
-    };
+    const mockStore = configureStore();
+    const initialState = 20;
+    const store = mockStore(initialState);
     const wallet = shallow(<Wallet store={store}/>).shallow();
 
     it('renders properly', () => {
@@ -15,7 +15,48 @@ describe('Wallet', () => {
     });
 
     it('displays the balance from store', () => {
-        console.log(wallet);
         expect(wallet.find('.balance').text()).toBe('Wallet balance: 20');
+    });
+
+    it('creates an input to deposit into or withdraw from the balance', () => {
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
+
+    describe('when the user types into the wallet input', () => {
+        const amount = '25';
+        
+        beforeEach(() => {
+            wallet.find('.input-wallet').simulate('change', {
+                target: {
+                    value: amount
+                }
+            });
+        });
+
+        it('updates the local wallet balance in state and converts it to a number', () => {
+            expect(wallet.state().amount).toBe(parseInt(amount, 10));
+        });
+
+        describe('and the user wants to make a deposit', () => {
+            beforeEach(() => {
+                store.clearActions();
+                wallet.find('.btn-deposit').simulate('click');
+            });
+
+            it('dispatches the deposit() it receives from props with local balance', () => {
+                expect(store.getActions()[0]).toEqual({ type: actionTypes.DEPOSIT, deposit: parseInt(amount, 10)});
+            });
+        });
+
+        describe('and the user wants to make a withdrawal', () => {
+            beforeEach(() => {
+                store.clearActions();
+                wallet.find('.btn-withdraw').simulate('click');
+            });
+
+            it('dispatches the withdrawal() it receives from props with local balance', () => {
+                expect(store.getActions()[0]).toEqual({ type: actionTypes.WITHDRAW, withdrawal: parseInt(amount, 10)});
+            });
+        });
     });
 });
